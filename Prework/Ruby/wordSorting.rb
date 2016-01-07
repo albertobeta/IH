c0cfc57234aece8cfdf75328c3f19df9a03a253f
Alberto Betella <alberto@betella.net>
2016-01-07 11:33:00 +0100

    comments improved

--- Prework/Ruby/wordSorting.rb
+++ Prework/Ruby/wordSorting.rb
@@ -3,11 +3,11 @@
 puts "Type a sentence:"
 string_to_split = gets.chomp()
 
-#1st we clean all non alphanumeric character, except spaces with a regexp
+#1st we clean all non alphanumeric characters, except spaces using regular expression
 #NB \s leaves spaces, /u is for UTF8 encoding
 string_cleaned = string_to_split.gsub(/[^\p{Alnum}\s]/u, '')
 
-#2nd we split the sentece. NB split() by default splits by space
+#2nd we split the sentence. NB split() by default splits by space
 elements = string_cleaned.split()
 
 #3rd we sort the elements of the array and ignore their case in the sorting
